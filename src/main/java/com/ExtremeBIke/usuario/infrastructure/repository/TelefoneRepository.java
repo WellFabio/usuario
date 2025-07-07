c88d4664c2f5227b081d777ebeecc730c43f465a
Welington <wellingtonfabi@gmail.com>
2025-07-07 20:24:48 -0300

    desenvoldido API

--- src/main/java/com/ExtremeBIke/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/ExtremeBIke/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,7 @@
 package com.ExtremeBIke.usuario.infrastructure.repository;
 
-import com.extremebike.aprendendospring.infrastructure.entity.Telefone;
+
+import com.ExtremeBIke.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
