6dbe5a8659f9020d522ba619583128445e4116bd
Welington <wellingtonfabi@gmail.com>
2025-07-09 20:53:51 -0300

    adicionado put de endereço e telefone

--- src/main/java/com/ExtremeBIke/usuario/infrastructure/entity/Usuario.java
+++ src/main/java/com/ExtremeBIke/usuario/infrastructure/entity/Usuario.java
@@ -17,7 +17,6 @@ import java.util.List;
 @Builder
 
     public class Usuario implements UserDetails {
-
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
